d031c187955ba6fa289ca294fef397146b7485cb
amir.hussain <amir.hussain@mobigesture.com>
2019-06-12 19:25:07 +0530

    route file with express router

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+//Define routes
+app.use('/api/users', require('./config/routes/api/users'));
+app.use('/api/auth', require('./config/routes/api/auth'));
+app.use('/api/profile', require('./config/routes/api/profile'));
+app.use('/api/posts', require('./config/routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
