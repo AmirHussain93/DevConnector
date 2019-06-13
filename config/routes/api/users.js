ffbe3a1d448110d2ab8b6a2a0321db3047566060
amir.hussain <amir.hussain@mobigesture.com>
2019-06-13 19:01:21 +0530

    Implemnted user registration

--- config/routes/api/users.js
+++ config/routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
 
 //@route  Post api/users
 //@desc   Register user
@@ -10,13 +13,50 @@ router.post('/',
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Please enter a valid email address').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-    ], (req, res) => {
+    ], async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() })
         }
-        console.log(req.body)
-        res.send('User route')
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email })
+
+            //If user exists
+            if (user) {
+                return res.status(400).json({ erros: [{ msg: 'User already exists' }] })
+            }
+
+            //Get users avatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+
+            //Encrypt password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+
+            //Return jsonwebtokens
+
+            res.send('User registered')
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send('Server error');
+        }
+
+
     });
 
 module.exports = router;
