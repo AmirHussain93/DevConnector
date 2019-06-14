2e67b261c2b98c956e897a39f8109692282c50df
amir.hussain <amir.hussain@mobigesture.com>
2019-06-14 15:00:41 +0530

    Implementing JWT

--- config/routes/api/users.js
+++ config/routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 
@@ -49,8 +51,22 @@ router.post('/',
             await user.save();
 
             //Return jsonwebtokens
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User registered')
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+            // res.send('User registered')
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server error');
